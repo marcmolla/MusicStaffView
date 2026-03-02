bbac3f4f8030d97d5eebf3a6c221dd8d1baf81e6
marcmolla <marcmolla@gmail.com>
2026-03-02 20:03:51 +0100

    Use SVG for common time C symbol

--- Sources/MusicStaffView/Elements/MusicTimeSignature+Element.swift
+++ Sources/MusicStaffView/Elements/MusicTimeSignature+Element.swift
@@ -12,30 +12,22 @@ import UIKit
 import Foundation
 import Music
 import SwiftUI
+import SVGParser
 
 extension MusicTimeSignature: MusicStaffViewElement {
     public func path(in frame: CGRect) -> CGPath {
-        // Si es 4/4, dibuja el símbolo C
         if beats == 4 && baseRhythm == .quarter {
-            return commonTimePath(in: frame)
+            return Self.commonTimePath
         }
         return CGPath(rect: .zero, transform: nil)
     }
     
-    private func commonTimePath(in frame: CGRect) -> CGPath {
-        let path = CGMutablePath()
-        let center = CGPoint(x: frame.midX, y: frame.midY)
-        let radius = min(frame.width, frame.height) * 0.4
-        let lineWidth = radius * 0.15
-        
-        // Dibuja un arco en forma de C
-        path.addArc(center: center, radius: radius, startAngle: .pi * 0.6, endAngle: .pi * 1.4, clockwise: false)
-        
-        return path.copy(strokingWithWidth: lineWidth, lineCap: .round, lineJoin: .round, miterLimit: 0)
-    }
+    static let commonTimePath: CGPath = {
+        try! SVGSingleElementContent(forResource: "opusCommonTime", withExtenstion: "svg").path!
+    }()
     
     public var aspectRatio: CGFloat {
-        return 0.8
+        return 1.0
     }
     
     public var heightInStaffSpace: CGFloat {
